41284d4a19b3f3c5fda115d87eada0d5797db623
Rusdi <ach.rusdhaini@gmail.com>
2024-08-27 10:45:47 +0700

    add exception on auth and change hibernate to update

--- src/main/java/com/enigmacamp/barbershop/service/Impl/AuthServiceImpl.java
+++ src/main/java/com/enigmacamp/barbershop/service/Impl/AuthServiceImpl.java
@@ -27,50 +27,60 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final RoleService roleService;
-    private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
+        private final UserRepository userRepository;
+        private final PasswordEncoder passwordEncoder;
+        private final RoleService roleService;
+        private final JwtService jwtService;
+        private final AuthenticationManager authenticationManager;
 
-    @Transactional(rollbackFor = Exception.class)
-    @Override
-    public RegisterResponse regiserUser(RegisterRequest request) throws DataIntegrityViolationException {
-        Role role = roleService.getOrCreate(UserRole.valueOf(request.getRole()));
+        @Transactional(rollbackFor = Exception.class)
+        @Override
+        public RegisterResponse regiserUser(RegisterRequest request) {
+                try {
+                        Role role = roleService.getOrCreate(UserRole.valueOf(request.getRole()));
 
-        Users user = Users.builder()
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(List.of(role))
-                .createdAt(System.currentTimeMillis())
-                .updateAt(System.currentTimeMillis())
-                .build();
+                        Users user = Users.builder()
+                                        .email(request.getEmail())
+                                        .password(passwordEncoder.encode(request.getPassword()))
+                                        .role(List.of(role))
+                                        .createdAt(System.currentTimeMillis())
+                                        .updateAt(System.currentTimeMillis())
+                                        .build();
 
-         userRepository.saveAndFlush(user);
+                        userRepository.saveAndFlush(user);
 
-         List<String> roles = user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+                        List<String> roles = user.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                                        .toList();
 
-        return RegisterResponse.builder()
-                .email(user.getUsername())
-                .roles(roles)
-                .build();
-    }
-    @Transactional(readOnly = true)
-    @Override
-    public LoginResponse login(LoginRequest request) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(),
-                        request.getPassword()
-                ));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        Users user = (Users) authentication.getPrincipal();
-        String token = jwtService.generateToken(user);
-        return LoginResponse.builder()
-                .userId(user.getId())
-                .email(user.getUsername())
-                .roles(user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
-                .token(token)
-                .build();
-    }
+                        return RegisterResponse.builder()
+                                        .email(user.getUsername())
+                                        .roles(roles)
+                                        .build();
+                } catch (Exception e) {
+                        throw new DataIntegrityViolationException("Email already exist");
+                }
+        }
+
+        @Transactional(readOnly = true)
+        @Override
+        public LoginResponse login(LoginRequest request) {
+                try {
+                        Authentication authentication = authenticationManager.authenticate(
+                                        new UsernamePasswordAuthenticationToken(
+                                                        request.getEmail(),
+                                                        request.getPassword()));
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                        Users user = (Users) authentication.getPrincipal();
+                        String token = jwtService.generateToken(user);
+                        return LoginResponse.builder()
+                                        .userId(user.getId())
+                                        .email(user.getUsername())
+                                        .roles(user.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                                                        .toList())
+                                        .token(token)
+                                        .build();
+                } catch (Exception e) {
+                        throw new RuntimeException("Email or password not correct");
+                }
+        }
 }
