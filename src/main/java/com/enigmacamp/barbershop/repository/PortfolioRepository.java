571675f6189d4752178900259ecc445c99dd5259
achrusdi <ach.rusdhaini@gmail.com>
2024-09-03 13:49:56 +0700

    refactor: :recycle: delete portofolio

--- src/main/java/com/enigmacamp/barbershop/repository/PortfolioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enigmacamp.barbershop.repository;
-
-import com.enigmacamp.barbershop.model.entity.Portfolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PortfolioRepository extends JpaRepository<Portfolio, String> {
-}
